b5f41b67a007ebff213c76c09d6b15ff22ef98db
Blair Englund <blair.englund@gmail.com>
2016-07-06 15:04:37 -0500

    more tweaks and tests

---
+++ articles/web_developer_terms.php
@@ -0,0 +1,20 @@
+<?php $pageID="web developer terms" ?>
+<?php $pageTitle = "web developer terms" ?>
+<?php include "articles_top.php" ?>
+
+<h1>Web Development Terminology</h1>
+
+<ul>
+	<li>HTTP - Hypertext Transfer Protocall. This is the system by which information is sent on the web. HTTP uses a request/response system for transfering data.</li>
+	<li>Request - A request is what is sent to a web server when we interact with a site on our browsers. Requests tell the web server what information it is that we'd like to receive back. For example, to navigate to a new page on a site we must send a request for that page. When the server receives a request it will send back the response. The two most-common requests are "GET", used to retrieve information from a server,  and "POST" which passes some type of data to the server.</li>
+	<li>Response - This is what comes from the web server following a request. The response might be a file containing text, html or other code to be displayed in the browser.</li>
+	<li>Query String Parameters - </li>
+	<li>Server - A network-connected computer that manages access to a centralized resource or service. Servers  manage databases, email, and file transfer among others.</li>
+	<li>Client - The user of a program.</li>
+	<li>Pre-Processor - A computer program that converts data into a form that is readable by another program.</li>
+</ul>
+
+
+
+
+<?php include "articles_bottom.php"
